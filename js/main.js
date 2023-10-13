0545a433042c6c141417efaf207a8dcdb625aa8c
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-13 15:10:31 +0200

    add FOR loop for cell elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 // console.log("funziona");
+
+const gridDOMElement = document.querySelector(".grid");
+console.log(gridDOMElement);
+
+const playButtonDOMElement = document.getElementById("btn-play");
+console.log(playButtonDOMElement);
+
+
+
+for (i = 0; i < 100; i++ ) {
+// console.log(i);
+const n = i + 1;
+// console.log(n);
+const cellHTML = `<div class="cell">${n}</div>`;
+// console.log(cellHTML);
+gridDOMElement.innerHTML += cellHTML;
+console.log(cellHTML);
+
+
+}
