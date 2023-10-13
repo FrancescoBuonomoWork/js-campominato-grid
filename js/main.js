5e3a4d772b39efc208a5a6da6ba5aedd776e99f1
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-10-13 15:32:25 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,39 @@
 // console.log("funziona");
 
 const gridDOMElement = document.querySelector(".grid");
-console.log(gridDOMElement);
+// console.log(gridDOMElement);
 
 const playButtonDOMElement = document.getElementById("btn-play");
-console.log(playButtonDOMElement);
+// console.log(playButtonDOMElement);
 
 
+playButtonDOMElement.addEventListener("click", function () {
+    // resettiamo la griglia 
+    gridDOMElement.innerHTML = "";
 
-for (i = 0; i < 100; i++ ) {
-// console.log(i);
-const n = i + 1;
-// console.log(n);
-const cellHTML = `<div class="cell">${n}</div>`;
-// console.log(cellHTML);
-gridDOMElement.innerHTML += cellHTML;
-console.log(cellHTML);
+    for (i = 0; i < 100; i++) {
+        // console.log(i);
+        const n = i + 1;
+        // console.log(n);
+        const cellHTML = `<div class="cell">${n}</div>`;
+        // console.log(cellHTML);
+        gridDOMElement.innerHTML += cellHTML;
+        // console.log(cellHTML);
+    }
+
+    const cellDOMElements = document.querySelectorAll(".cell");
+    // console.log(cellDOMElements);
+    // console.log(cellDOMElements.length);
+    for (i = 0; i < cellDOMElements.length; i++) {
+        const currentCellDOMElement = cellDOMElements[i];
+        // console.log(currentCellDOMElement);
+
+        currentCellDOMElement.addEventListener("click", function () {
+            currentCellDOMElement.classList.add("bg-lightblue");
+            console.log(currentCellDOMElement);
 
 
-}
+        })
+    }
+
+})
